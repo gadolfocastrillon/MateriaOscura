52aa93473aa584e8ab1cb30d3b48485d96c64a41
gadolfocastrillon <gadolfo.castrillon@udea.edu.co>
2023-04-11 22:41:37 -0500

    Actualización del programa newlikelihood.py y sus respectivas graficos y datos con resultados similares a los planteados en el articulo del modelo Z3

--- SingleteEscalar/micromegas_5.3.41/Z3Model-singlete/newlikelihood.py
+++ SingleteEscalar/micromegas_5.3.41/Z3Model-singlete/newlikelihood.py
@@ -1,16 +1,27 @@
 import numpy as np 
 import subprocess 
 from scipy.optimize import differential_evolution 
+from scipy.stats import chi2
 import matplotlib.pyplot as plt 
 import time 
-from scipy.stats import chi2
 
-dim = 3
+
+
+dim = 4
 sltns = 0 
+min_chi_sq = 0.
+alpha = 0.05
+critical_chi_sq = chi2.isf(alpha,2)
+data_vector = [] 
 
 min_chi_sq = 0.
 alpha = 0.05
 critical_chi_sq = chi2.isf(alpha,2)
+
+# Color style for output sample points
+de_pts = "#91bfdb" # Diver scan
+rn_pts = "#fc8d59" # Random scan
+gd_pts = "#ffffbf" # Grid scan
 
 def writer(file,dictionary):
 	data1=open(file,'w')
@@ -18,10 +29,12 @@ def writer(file,dictionary):
 		data1.write("%s %s\n"%items)
 	data1.close()
 
+
 def writer2(x,file):
 	data = open(file,'w') 
+	#me guarda los datos de masa, lahs, mu3 y cross section
 	for i in x: 
-		texto = str(i[0]) + " " + str(i[1]) + " " +str(i[2])+"\n"
+		texto = str(i[0]) + " " + str(i[1]) + " " +str(i[2])+ " " + str(i[3]) +"\n"
 		data.write(texto)
 	data.close()
 
@@ -30,10 +43,18 @@ def samples_inside(x,chi_sq):
     inside = delta_chi_sq <= critical_chi_sq
     return x[:, inside]
 
+#Me halla la cross section
+def csection(): 
+	cs = 0.0
+	COMMAND = "grep 'proton  SI' temporal.dat | awk '{print $3}'"
+	dato = subprocess.getoutput(COMMAND)
+	if(len(dato)>0):
+		cs = float(dato)
+	else: 
+		cs = -1 
+	return cs
+
 def omega(X):
-	m = X[0]
-	lam = X[1]
-	u3 = X[2]
 	#---------------- Rutas de los archivos -------------------------------
 	ruta = 'data.dat' #Ruta para guardar el archivo.
 	rutaG = './main data.dat > temporal.dat' #Ruta para ejecutar micromegas 
@@ -41,9 +62,10 @@ def omega(X):
 	
 	#------Diccionario con los datos del archivo. -------------------------
 	data = {'Mh':125, 'laphi':0.07,'laSH1':0.1,'Mp1':300,'mu32':1000,'Mtop':173.2} 
-	data['Mp1'] = m #Valores de la masa de la particulas.
-	data['laSH1'] = lam #Valores de laSH
-	data['mu32'] = u3 #Valores de mu3
+	data['Mp1'] = X[1] #Valores de la masa de la particulas.
+	data['laSH1'] = X[0] #Valores de laSH
+	data['mu32'] = X[2] #Valores de mu3
+	#print(data)
 	#----------------------------------------------------------------------
 
 	#--------------------Sistema de escritura en el archivo----------------
@@ -61,32 +83,46 @@ def omega(X):
 	dato = subprocess.getoutput(COMMAND) #ejecutar el comando desde la terminal 
 	if (len(dato)>0): 
 		omeg = float(dato) 
+		'''
+		if ((omeg <0.130)and(omeg>0.110)):
+			cs = csection()
+			#Me guarda los puntos de interes para el problema fisico
+			data_vector.append([omeg,X[1],X[2],X[3],cs]) 
+			sltns+=1
+		'''
 	else: 
 		omeg = -1 
+	return omeg
 	#----------------------------------------------------------------------
-
-	return omeg #Retorna el valor de la densidad reliquia.
 
 #-------------------------Función gaussiana definida en el articulo --------
 
 def gaussian(X):
 	x = omega(X)
-	sigma = ((0.1*x)**2 + (0.001)**2)**(0.5) #Defino el sigma 
+	sigma = np.sqrt((0.1*x)**2 + (0.001)**2) #Defino el sigma 
 	xobs = 0.120 #densidad reliquia observable
-	return (x-xobs)/sigma  
+	return ((x-xobs)/sigma)**2 #Gaussiana
+
 #---------------------------------------------------------------------------
 
 #------------------- Método para differential evolution --------------------
 def de_scan(dim,round_to_nearest=None): 
 	x = [] 
-	chi_sq = [] 
-	bounds = [(0,1000),(1e-4,1),(0,10000)] #ligaduras
-
-	def objective(x_): 
+	chi_sq = []
+	cs =[] 
+	#lambdaSH, Masa, Mu3
+	bounds = [(1e-4,1),(100,200),(0,10000)] #ligaduras
+	def objective(x_):
 		chi_sq_ = gaussian(x_)
 		chi_sq.append(chi_sq_)
 		x.append(x_)
+		cross = csection()
+		cs.append(cross)
+		#print(x_,cross)
+		if (len(x) % 10000 == 0):
+			print(len(x))
 		return chi_sq_
+
 	#strategy condiciones a cumplir la estrategia 
 	#popsize cantidad de individuos cantidatos a generar 
 	#tol tolerancia del problema? error del problema? 
@@ -101,48 +137,62 @@ def de_scan(dim,round_to_nearest=None):
 	
 	differential_evolution(objective, bounds,
                            strategy='rand1bin', maxiter=None,
-                           popsize=50, tol=0.01, mutation=(0.7, 1.99999), recombination=0.15,
-                           polish=False, seed=10)
-	'''
-	datos = differential_evolution(gaussian, bounds,
-                           strategy='rand1bin', maxiter=None,
-                           popsize=50, tol=0.01, mutation=(0.7, 1.99999), recombination=0.15,
-                           polish=False, seed=10)
+                           popsize=100, tol=0.01, mutation=(0.7, 1.99999), recombination=0.15,
+                           polish=False, seed=120)
 	
-	'''
-	
-	
+	valor = np.array(x).T
+	datos=[valor[i] for i in range(len(valor))]
+	datos.append(cs) #Agrego cross section a 
+
 	if round_to_nearest is not None: 
 		len_x = len(x)
 		keep_n = len_x - (len_x %round_to_nearest)
 		x = x[:keep_n]
 		chi_sq = chi_sq[:keep_n] 
-	
-	#print(x)
-	#print(np.array(x))
-	#return samples_inside(np.array(x).T, np.array(chi_sq)),len(x)
-	return x
-	
-	'''
-	print(datos.x)
-	data2=open("informacion.txt",'w')
-	data2.write(datos.x)
-	#data2.close()
-	return datos
-	'''
-#---------------------------------------------------------------------------
 
+	return samples_inside(np.array(datos),np.array(chi_sq)),len(x)
+	#return samples_inside(np.array(x).T, np.array(chi_sq)),len(x)
+	#return x
+#---------------------------------------------------------------------------
+'''
+Pendiente: 
+Falta guardar los datos, pasar los valores de picmetro² a cm² con 1picometro = 1e-10 cm 
+pico² = 1e-20 cm² 
+verificar que se guardan los datos del cross section. 
+
+'''
 #------------------------------Uso en la función main-----------------------
 if __name__ == '__main__': 
-	#np.random.seed(seed) 
-	plt.figure(figsize=(10,7))
+	fig, ax = plt.subplots(figsize=(10,7)) 
 	print("Running de_scan") 
 	t0 = time.time()
-	#x, calls = de_scan(dim,round_to_nearest=1000) 
-	#x = de_scan(dim,round_to_nearest=10000) 
-	x = de_scan(dim,round_to_nearest=1000)
-	writer2(x,"AlmacenarDatos/datosLikelihood3.txt") 
+	x, calls  = de_scan(dim) 	
+	#calls me devuelve el tamaño de los datos 
+	#x es un arreglo con: x[0]:lambdahs, x[1]: MasaS, x[2]: Mu3, x[3]: cross section.
+	writer2(x.T,"AlmacenarDatos/datosLikelihood-modificado-P100.txt") 
 	de_time = time.time() - t0 
-	print("Plotting de_scan") 
 	print(r'Tiempo de ejecución : '+ str(de_time)) #Imprime el tiempo que demora en ejecutar el progama.
+	print("Finalizado")
+	'''
+	plt.scatter(x[3], x[0], s=30, edgecolor='0.05',
+                linewidth=0.25, alpha=1.0, facecolor=de_pts,
+                label=r'Differential evolution: \num{{{:d}}} out of \num{{{:d}}} points'
+                .format(x.shape[1], calls))
+	plt.title("Calculo de la cross section apartir de una likelihood gaussiana") 
+	plt.xlabel("Masa") 
+	plt.ylabel("Seccion transversal") 
+	plt.savefig("Densidad reliquia.svg")
+	plt.show()
+	'''
+	#ax.set_ylabel('Y label',loc='top')
+	#ax.set_xlabel('XLabel',loc='left') 
+	ax.set_xscale('log')
+	ax.set_yscale('log')
+	im = ax.scatter(x[1],x[3],c=x[2],cmap='viridis')
+	fig.colorbar(im)
+	plt.title("Singlete escalar con simetría $Z_{3}$")
+	plt.ylabel("Cross Section ($\sigma_{SI}$)")
+	plt.xlabel("Mass DM (GeV)")
+	plt.savefig('AlmacenarDatos/mapeo_de_datos_likelihood6.svg')
+	plt.show()
 #---------------------------------------------------------------------------
